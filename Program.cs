6190589dd812afb0b936e490cd4379b5874962a4
Yash Sharma <yash.sharma@zeuslearning.mumbai>
2026-06-15 12:36:36 +0530

    shifted to log4net

--- Program.cs
+++ Program.cs
@@ -56,6 +56,9 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySQL(
         connectionString
     ));
+    
+// log4net
+builder.Logging.AddLog4Net("log4net.config");
 
 var app = builder.Build();
 
